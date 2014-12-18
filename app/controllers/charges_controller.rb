5a0c3530922127404a9eeef9d83e10b2eed19638
Scott Wilson <ScottWilson1020@gmail.com>
2014-12-18 13:42:13 -0600

    fixed the redirecting problem, everything should be good to go

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,16 +1,18 @@
 class ChargesController < ApplicationController
 
 	def create
-		 
+		  # Amount in cents
+		  @amount = 500
+
 		  customer = Stripe::Customer.create(
-		    :email => params[:stripeEmail],
+		    :email => 'example@stripe.com',
 		    :card  => params[:stripeToken]
 		  )
 
 		  charge = Stripe::Charge.create(
 		    :customer    => customer.id,
-		    :amount      => params[:amount],
-		    :description => 'Growth Hacking Crash Course',
+		    :amount      => @amount,
+		    :description => 'Rails Stripe customer',
 		    :currency    => 'usd'
 		  )
 
